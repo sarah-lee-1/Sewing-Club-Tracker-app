d8dc8c75c4a15126fdba2546cffbfacc44752ec1
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-23 10:53:19 -0600

    Chore: Add styling via App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+     <table>
+       <thead>
+         <tr>Name</tr>
+         <tr>Address</tr>
+         <tr>Phone Number</tr>
+         <tr>Email</tr>
+         <tr></tr>
+       </thead>
+       <body>
+         <tr>
+           <td>Sally Smith</td>
+           <td>132 N Lake Dr</td>
+           <td>Whitefish Bay</td>
+           <td>sallysmith@aol.com</td>
+           <td></td>
+         </tr>
+       </body>
+     </table>
     </div>
   );
 }
