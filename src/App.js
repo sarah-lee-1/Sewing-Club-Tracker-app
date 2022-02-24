f61bba641426afce1d6d13e310676e9f00ce564c
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-24 10:44:41 -0600

    Perf: Create Add member function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css"; 
+import data from "./mock-data.json"
 
 function App() {
+  const [contacts, setContacts] = useState(data);
+  const [addFormData, setFormData] = useState({
+    fullName: '',
+    address: '',
+    phoneNumber: '',
+    email: ''
+  })
+
   return (
     <div className="app-container">
      <table>
@@ -13,15 +22,25 @@ function App() {
          <tr></tr>
        </thead>
        <body>
-         <tr>
-           <td>Sally Smith</td>
-           <td>132 N Lake Dr</td>
-           <td>Whitefish Bay</td>
-           <td>sallysmith@aol.com</td>
+         {contacts.map((contact) => (
+           <tr>
+           <td>{contact.fullName}</td>
+           <td>{contact.address}</td>
+           <td>{contact.phoneNumber}</td>
+           <td>{contact.email}</td>
            <td></td>
          </tr>
+         ))}
        </body>
      </table>
+          <h2>Add a Contact</h2>
+          <form>
+            <input type="text" name="fullName" required="required" placeholder="Enter a name..."/>
+            <input type="text" name="address" required="required" placeholder="Enter an address..."/>
+            <input type="text" name="phoneNumber" required="required" placeholder="Enter a phone number..."/>
+            <input type="email" name="email" required="required" placeholder="Enter an email..."/>
+            <button typ="submit">Add</button>
+          </form>
     </div>
   );
 }
